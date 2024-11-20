1a62e91cb3d6fa5a597a1995b3701177ecf36059
RAlexandervazy <118690963+RAlexandervazy@users.noreply.github.com>
2024-11-19 23:42:05 -0600

    Update streamlit_app.py
    llamada a APi

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,9 @@ if ingredients_List:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}", icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://SmoothieFroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
